a229b2cfaf480c16854bb252ef84b34f0bc60ad9
qqa00165 <qqa00165@gmail.com>
2017-09-30 14:38:40 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n;
+        System.out.print("Input:");
+        n=in.nextInt();
+        System.out.printf("%d\n",n*9/5+32);
+
+
+        // write your code here
+    }
+}
